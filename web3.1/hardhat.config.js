a92ef53869cf1cbf04088a062385d4d299302b2e
DC-21 <cholahkuboko@gmail.com>
2023-06-18 10:05:57 +0200

    first commit

--- web3.1/hardhat.config.js
+++ web3.1/hardhat.config.js
@@ -33,6 +33,14 @@ module.exports = {
   },
   solidity: {
     version: "0.8.17",
+    defaultNetwork: 'goerli',
+    networks: {
+      hardhat:{},
+      goerli: {
+        url: 'https://rpc.ankr.com/eth_goerli',
+        accounts: ['0x${process.env.PRIVATE_KEY']
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
