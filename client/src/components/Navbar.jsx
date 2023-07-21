03ee122c4539f7bb7a675f6bd948e5d85e276705
DC-21 <cholahkuboko@gmail.com>
2023-07-21 19:39:47 +0200

    first commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,13 +5,14 @@ const Navbar = () => {
     return (
         <div className='top-4 pt-4 w-full h-20'>
             <div className='flex py-3 px-2 w-full justify-between items-center'>
-                <div className='flex justify-center gap-8'>
+                <div className='flex items-center justify-center gap-8'>
                     <div className="w-14 h-4 flex justify-center items-center ml-1">
                         <img className='rounded' src={logo} alt='logo' />
                     </div>
-                    <div className='flex bg-[#0e0e10] justify-center'>
+                    <div className='flex justify-center items-center'>
                         <input
                             type='text'
+                            className='rounded h-9 justify-center'
                             placeholder='search' />
                     </div>
                 </div>
