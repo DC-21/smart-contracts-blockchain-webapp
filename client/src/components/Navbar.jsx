51e5de002170e8a62a7cee8d5cb50d91677363b5
DC-21 <cholahkuboko@gmail.com>
2023-07-21 19:51:35 +0200

    first commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import logo from '../images/dice.png';
 
 const Navbar = () => {
     return (
-        <div className='top-4 pt-4 w-full h-20'>
-            <div className='flex py-3 ml-2 mr-2 rounded justify-between items-center  bg-[#0e0e10]'>
+        <div className='top-4 pt-2 w-full h-20'>
+            <div className='flex py-4 ml-2 mr-2 rounded justify-between items-center  bg-[#0e0e10]'>
                 <div className='flex items-center justify-center gap-8'>
                     <div className="w-14 h-4 flex justify-center items-center ml-2">
                         <img className='rounded' src={logo} alt='logo' />
@@ -12,12 +12,17 @@ const Navbar = () => {
                     <div className='flex justify-center items-center'>
                         <input
                             type='text'
-                            className='rounded h-9 justify-center'
+                            className='rounded h-9justify-center'
                             placeholder='search' />
                     </div>
                 </div>
-                <div className="flex-none w-14 flex justify-center items-center">
-                    <ion-icon name="logo-firefox"></ion-icon>
+                <div className='flex items-center justify-center gap-8'>
+                    <div>
+                        <button>Connect</button>
+                    </div>
+                    <div className="flex-none w-14 flex justify-center items-center">
+                        <ion-icon name="logo-firefox"></ion-icon>
+                    </div>
                 </div>
             </div>
         </div>
