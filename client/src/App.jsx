a5b2803fffc2d006e9011190eef7831a64c8842b
DC-21 <cholahkuboko@gmail.com>
2023-07-21 15:47:53 +0200

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='h-screen w-full bg-blue-950'>
+      <Navbar/>
       <Sidebar/>
     </div>
   )
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='top-4 pt-4 w-full h-20 bg-[#0e0e10]'>
+        <div className='bg-white py-7 px-4 w-full'></div>
+    </div>
+  )
+}
+
+export default Navbar;
