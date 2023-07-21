0e8d7ac95ff28d417c5fb1e5441b5a01dfe3c82d
DC-21 <cholahkuboko@gmail.com>
2023-07-21 18:10:49 +0200

    first commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,21 +2,28 @@ import React from 'react';
 import logo from '../images/dice.png';
 
 const Navbar = () => {
-  return (
-    <div className='top-4 pt-4 w-full h-20'>
-      <div className='bg-white flex py-5 px-2 w-full justify-center items-center'>
-        <div className="flex-none w-14 h-4 flex justify-center items-center ml-1">
-          <img className='rounded' src={logo} alt='logo' />
+    return (
+        <div className='top-4 pt-4 w-full h-20'>
+            <div className='bg-white flex py-5 px-2 w-full justify-center items-center'>
+                <div className="flex-none w-14 h-4 flex justify-center items-center ml-1">
+                    <img className='rounded' src={logo} alt='logo' />
+                </div>
+                <div className="grow h-4 flex justify-center items-center">
+                    <input
+                    type='text'
+                    placeholder='search'
+                    className='h-8 text-center'
+                    />
+                </div>
+                <div className="grow h-4 flex justify-center items-center">
+                    02
+                </div>
+                <div className="flex-none w-14 h-4 flex justify-center items-center">
+                    03
+                </div>
+            </div>
         </div>
-        <div className="grow h-4 flex justify-center items-center">
-          02
-        </div>
-        <div className="flex-none w-14 h-4 flex justify-center items-center">
-          03
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
