bb26f69aa35deb697aa7a548da68f3bc44fb17db
DC-21 <cholahkuboko@gmail.com>
2023-07-21 18:37:17 +0200

    first commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import logo from '../images/dice.png';
 
 const Navbar = () => {
-  return (
-    <div className='top-4 pt-4 w-full h-20'>
-      <div className='bg-white flex py-5 px-2 w-full justify-between items-center'>
-        <div className="w-14 h-4 flex justify-center items-center ml-1">
-          <img className='rounded' src={logo} alt='logo' />
+    return (
+        <div className='top-4 pt-4 w-full h-20'>
+            <div className='bg-white flex py-5 px-2 w-full justify-between items-center'>
+                <div className="w-14 h-4 flex justify-center items-center ml-1">
+                    <img className='rounded' src={logo} alt='logo' />
+                </div>
+                <div className="flex-none w-14 flex justify-center items-center rounded-full">
+                    <ion-icon name="logo-firefox"></ion-icon>
+                </div>
+            </div>
         </div>
-        <div className="flex-none w-14 h-4 flex justify-center items-center">
-        <ion-icon name="logo-firefox"></ion-icon>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
