9899f343e3fa7cb382a6c17f3fdcf22dd956e6e8
DC-21 <cholahkuboko@gmail.com>
2023-07-21 18:38:58 +0200

    first commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,11 +4,11 @@ import logo from '../images/dice.png';
 const Navbar = () => {
     return (
         <div className='top-4 pt-4 w-full h-20'>
-            <div className='bg-white flex py-5 px-2 w-full justify-between items-center'>
+            <div className='bg-[#4e515a] flex py-3 px-2 w-full justify-between items-center'>
                 <div className="w-14 h-4 flex justify-center items-center ml-1">
                     <img className='rounded' src={logo} alt='logo' />
                 </div>
-                <div className="flex-none w-14 flex justify-center items-center rounded-full">
+                <div className="flex-none w-14 flex justify-center items-center">
                     <ion-icon name="logo-firefox"></ion-icon>
                 </div>
             </div>
