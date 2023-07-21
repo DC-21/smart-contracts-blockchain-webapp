e2d4764d0c378fd6f9665157e1c3118491c3fa22
DC-21 <cholahkuboko@gmail.com>
2023-07-21 14:47:36 +0200

    first commit

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const Sidebar = () => {
   return (
-    <div className='w-20 h-[700px] bg-[#0e0e10] ml-2'></div>
-  )
-}
+    <div className='w-20 rounded-md pt-10 flex fixed bottom-0 left-2 h-[600px] bg-[#0e0e10]'></div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
