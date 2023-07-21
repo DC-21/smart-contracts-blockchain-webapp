12850e534fbecb8dccd79a251105b86bc665e107
DC-21 <cholahkuboko@gmail.com>
2023-07-21 15:56:15 +0200

    first commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='top-4 pt-4 w-full h-20 bg-[#0e0e10]'>
-        <div className='bg-white py-7 px-4 w-full'></div>
+    <div className='top-4 pt-4 w-full h-20'>
+        <div className='bg-white py-7 px-2 w-full'></div>
     </div>
   )
 }
