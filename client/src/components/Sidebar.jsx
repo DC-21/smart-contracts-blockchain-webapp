ef8fce7df4e4463508a8473ddd0ec4d40ed5ee85
DC-21 <cholahkuboko@gmail.com>
2023-07-21 15:18:39 +0200

    first commit

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,9 +3,21 @@ import React from 'react';
 const Sidebar = () => {
   return (
     <div className='w-16 rounded-lg pt-10 flex fixed bottom-10 left-2 h-[550px] bg-[#0e0e10]'>
-        <div className='w-full h-full flex flex-col'>
-            <a className='bg-white justify-center'>
+        <div className='w-full justify-center h-full flex flex-col'>
+            <a className='bg-white flex justify-center'>
             <ion-icon name="grid-outline"></ion-icon>
+            </a>
+            <a className='bg-white flex justify-center'>
+            <ion-icon name="person-circle-outline"></ion-icon>
+            </a>
+            <a className='bg-white flex justify-center'>
+            <ion-icon name="person-circle-outline"></ion-icon>
+            </a>
+            <a className='bg-white flex justify-center'>
+            <ion-icon name="person-circle-outline"></ion-icon>
+            </a>
+            <a className='bg-white flex justify-center'>
+            <ion-icon name="download-outline"></ion-icon>
             </a>
         </div>
     </div>
