8bb7c769299fc07b0f41e1a3adfdd7c40e6eb0a1
DC-21 <cholahkuboko@gmail.com>
2023-07-21 18:41:13 +0200

    first commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,8 +5,15 @@ const Navbar = () => {
     return (
         <div className='top-4 pt-4 w-full h-20'>
             <div className='bg-[#4e515a] flex py-3 px-2 w-full justify-between items-center'>
-                <div className="w-14 h-4 flex justify-center items-center ml-1">
-                    <img className='rounded' src={logo} alt='logo' />
+                <div className='flex justify-evenly'>
+                    <div className="w-14 h-4 flex justify-center items-center ml-1">
+                        <img className='rounded' src={logo} alt='logo' />
+                    </div>
+                    <div className='flex'>
+                        <input
+                            type='text'
+                            placeholder='search' />
+                    </div>
                 </div>
                 <div className="flex-none w-14 flex justify-center items-center">
                     <ion-icon name="logo-firefox"></ion-icon>
