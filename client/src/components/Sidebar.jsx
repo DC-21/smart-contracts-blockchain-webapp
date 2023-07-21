8952da10810e0cecd8dad7f28d9f93ccd9bdf64d
DC-21 <cholahkuboko@gmail.com>
2023-07-21 15:23:40 +0200

    first commit

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 
 const Sidebar = () => {
-  return (
-    <div className='w-16 rounded-lg pt-10 flex fixed bottom-10 left-2 h-[550px] bg-[#0e0e10]'>
-        <div className='w-full justify-center h-full flex flex-col'>
-            <a className='bg-white flex justify-center'>
-            <ion-icon name="grid-outline"></ion-icon>
-            </a>
-            <a className='bg-white flex justify-center'>
-            <ion-icon name="person-circle-outline"></ion-icon>
-            </a>
-            <a className='bg-white flex justify-center'>
-            <ion-icon name="person-circle-outline"></ion-icon>
-            </a>
-            <a className='bg-white flex justify-center'>
-            <ion-icon name="person-circle-outline"></ion-icon>
-            </a>
-            <a className='bg-white flex justify-center'>
-            <ion-icon name="download-outline"></ion-icon>
-            </a>
+    return (
+        <div className='w-16 rounded-lg pt-10 flex fixed bottom-10 left-2 h-[550px] bg-[#0e0e10]'>
+            <div className='w-full justify-center h-full flex flex-col'>
+                <a className='bg-white flex justify-center'>
+                    <ion-icon name="grid-outline"></ion-icon>
+                </a>
+                <a className='bg-white flex justify-center'>
+                    <ion-icon name="person-circle-outline"></ion-icon>
+                </a>
+                <a className='bg-white flex justify-center'>
+                <ion-icon name="notifications-circle-outline"></ion-icon>
+                </a>
+                <a className='bg-white flex justify-center'>
+                    <ion-icon name="card-outline"></ion-icon>
+                </a>
+                <a className='bg-white flex justify-center'>
+                    <ion-icon name="person-circle-outline"></ion-icon>
+                </a>
+                <a className='bg-white flex justify-center'>
+                    <ion-icon name="download-outline"></ion-icon>
+                </a>
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default Sidebar;
