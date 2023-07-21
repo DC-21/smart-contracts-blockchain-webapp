7e5ecb22d19604b4b5ad8933900c541d0a3d8f0f
DC-21 <cholahkuboko@gmail.com>
2023-07-21 15:59:22 +0200

    first commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div className='top-4 pt-4 w-full h-20'>
-        <div className='bg-white py-7 px-2 w-full'></div>
-    </div>
-  )
+    return (
+        <div className='top-4 pt-4 w-full h-20'>
+            <div className='bg-white flex py-5 px-2 w-full'>
+                <div class="flex-none w-14 h-4">
+                    01
+                </div>
+                <div class="grow h-4">
+                    02
+                </div>
+                <div class="flex-none w-14 h-4">
+                    03
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar;
