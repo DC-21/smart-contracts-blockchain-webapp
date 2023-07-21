0ec02933db727faa63f69b6672002f4e94609e74
DC-21 <cholahkuboko@gmail.com>
2023-07-21 18:42:44 +0200

    first commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import logo from '../images/dice.png';
 const Navbar = () => {
     return (
         <div className='top-4 pt-4 w-full h-20'>
-            <div className='bg-[#4e515a] flex py-3 px-2 w-full justify-between items-center'>
-                <div className='flex justify-evenly'>
+            <div className='flex py-3 px-2 w-full justify-between items-center'>
+                <div className='flex justify-center gap-8'>
                     <div className="w-14 h-4 flex justify-center items-center ml-1">
                         <img className='rounded' src={logo} alt='logo' />
                     </div>
