e6daf93c4eb0e610f56b64801400a955dd9c1e39
DC-21 <cholahkuboko@gmail.com>
2023-07-21 15:34:16 +0200

    first commit

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,26 +2,26 @@ import React from 'react';
 
 const Sidebar = () => {
   return (
-    <div className='w-16 rounded-lg pt-4 flex flex-col justify-between fixed bottom-10 left-2 h-[550px] bg-[#0e0e10]'>
-      <div className='w-full h-full flex gap-6 top-0 flex-col'>
-        <a className='bg-white flex justify-center'>
+    <div className='w-16 rounded-lg pt-4 pb-4 flex flex-col justify-between fixed bottom-10 left-2 h-[550px] bg-[#0e0e10]'>
+      <div className='w-full h-full flex gap-3 top-0 flex-col'>
+        <a className='flex justify-center'>
           <ion-icon name="grid-outline"></ion-icon>
         </a>
-        <a className='bg-white flex justify-center'>
+        <a className='flex justify-center'>
           <ion-icon name="person-circle-outline"></ion-icon>
         </a>
-        <a className='bg-white flex justify-center'>
+        <a className='flex justify-center'>
           <ion-icon name="notifications-circle-outline"></ion-icon>
         </a>
-        <a className='bg-white flex justify-center'>
+        <a className='flex justify-center'>
           <ion-icon name="card-outline"></ion-icon>
         </a>
-        <a className='bg-white flex justify-center'>
+        <a className='flex justify-center'>
           <ion-icon name="person-circle-outline"></ion-icon>
         </a>
       </div>
-      <div className='flex justify-center'>
-        <a className='bg-white flex justify-center'>
+      <div className='flex w-full flex-col justify-center'>
+        <a className='flex justify-center'>
           <ion-icon name="download-outline"></ion-icon>
         </a>
       </div>
