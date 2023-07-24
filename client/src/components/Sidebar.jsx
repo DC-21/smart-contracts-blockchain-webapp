c3daff0b032f675a39af6af281aab55f528d2f1f
DC-21 <cholahkuboko@gmail.com>
2023-07-24 09:37:49 +0200

    first commit

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,27 +4,27 @@ const Sidebar = () => {
     return (
         <div className='w-16 rounded-lg pt-4 pb-4 md:flex hidden flex-col justify-between fixed bottom-6 left-2 h-[540px] bg-[#06123d] shadow-2xl shadow-black '>
             <div className='w-full h-full flex gap-3 top-0 flex-col'>
-                <a className='flex justify-center'>
+                <a className='flex justify-center cursor-pointer'>
                     <ion-icon name="grid-outline"></ion-icon>
                 </a>
-                <a className='flex justify-center'>
+                <a className='flex justify-center cursor-pointer'>
                     <ion-icon name="megaphone-outline"></ion-icon>
                 </a>
-                <a className='flex justify-center'>
+                <a className='flex justify-center cursor-pointer'>
                     <ion-icon name="wallet-outline"></ion-icon>
                 </a>
-                <a className='flex justify-center'>
+                <a className='flex justify-center cursor-pointer'>
                     <ion-icon name="cash-outline"></ion-icon>
                 </a>
-                <a className='flex justify-center'>
+                <a className='flex justify-center cursor-pointer'>
                     <ion-icon name="person-circle-outline"></ion-icon>
                 </a>
-                <a className='flex justify-center'>
+                <a className='flex justify-center cursor-pointer'>
                     <ion-icon name="log-out-outline"></ion-icon>
                 </a>
             </div>
             <div className='flex w-full flex-col justify-center'>
-                <a className='flex justify-center'>
+                <a className='flex justify-center cursor-pointer'>
                     <ion-icon name="sunny-outline"></ion-icon>
                 </a>
             </div>
