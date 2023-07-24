12d88904b8d39dbfdc879e95c882c21b5963e4c5
DC-21 <cholahkuboko@gmail.com>
2023-07-24 16:04:14 +0200

    first commit

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -20,7 +20,7 @@ const Create = () => {
       <div className='bg-[#374674] flex justify-center items-center rounded-[10px] md:mt-6 mt-4 md:p-[20px] p-4'>
         <h1 className='text-white font-bold leading-[30px] md:text-[35px] sm:text-[16px]'>Start a Campaign</h1>
       </div>
-      <form className='w-full flex flex-col mt-6 mb-6 gap-4'>
+      <form className='w-full justify-center mx-auto flex flex-col mt-6 mb-6 gap-4'>
         <div className='flex flex-wrap gap-0'>
           <FormField
             labelName='Your Name *'
@@ -46,19 +46,19 @@ const Create = () => {
             handleChange={() => { }}
           />
         </div>
-        <div className='justify-center w-full flex items-center gap-4'>
-          <a>
+        <div className='justify-center w-full flex items-center'>
+          <a className='gap-4 bg-[#374674] flex justify-center items-center p-4 rounded-[10px]'>
             <svg
               viewBox="0 0 24 24"
               fill="currentColor"
-              height="4.5em"
-              width="4.5em"
+              height="3.5em"
+              width="3.5em"
             >
               <path fill="none" d="M0 0h24v24H0z" />
               <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 100-16 8 8 0 000 16zm-3.5-6H14a.5.5 0 100-1h-4a2.5 2.5 0 110-5h1V6h2v2h2.5v2H10a.5.5 0 100 1h4a2.5 2.5 0 110 5h-1v2h-2v-2H8.5v-2z" />
             </svg>
+            <p className='font-bold text-[28px] text-white'>You will get 100% of the raised amount by your campaign</p>
           </a>
-          <p>You will get 100% of the raised amount</p>
         </div>
       </form>
     </div>
