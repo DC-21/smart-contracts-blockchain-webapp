934108072fe16be75432cd816b50491052fdc508
DC-21 <cholahkuboko@gmail.com>
2023-07-24 10:20:18 +0200

    first commit

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ethers } from 'ethers';
 
 const Create = () => {
+    const navigate = useNavigate();
   return (
     <div className='justify-center md:w-[93%] w-full h-auto bg-black md:ml-20 ml-0 mt-3'>
         <p className='text-white text-left'>create campaign</p>
