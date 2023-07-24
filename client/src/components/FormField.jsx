f0cb03462e4083964d22f0b2de9994dcac3ee70e
DC-21 <cholahkuboko@gmail.com>
2023-07-24 12:14:44 +0200

    first commit

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -3,7 +3,24 @@ import React from 'react'
 const FormField = ({ labelName, placeholder, inputType, isTextArea, value, handleChange }) => {
   return (
     <label className='justify-center items-center flex-1 w-full flex-col flex'>
-        {labelName && (<span>{labelName}</span>)}
+        {labelName && (<span className='text-[#a4a5b8]'>{labelName}</span>)}
+        {isTextArea ? (
+            <textarea
+            required
+        value={value}
+        type={inputType}
+        rows={10}
+        placeholder={placeholder}
+        className='py-[15px] px-[15px] sm:px-[25px] rounded-[5px] sm:min-w-[300px]'
+        />
+        ):(<input
+        required
+        value={value}
+        type={inputType}
+        steps='0.1'
+        placeholder={placeholder}
+        className='py-[15px] px-[15px] sm:px-[25px] rounded-[5px] sm:min-w-[300px]'
+        />)}
     </label>
   )
 }
--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -17,26 +17,33 @@ const Create = () => {
   return (
     <div className='justify-center items-center flex flex-col rounded-[10px] md:w-[92%] w-full md:h-screen h-auto bg-[#101b42] md:ml-[7%] ml-0 md:mr-[7%] mr-0 mt-3'>
       {isLoading && 'Loader...'}
-      <div className='bg-[#374674] flex justify-center items-center rounded-[10px] md:mt-6 mt-4 p-[20px]'>
+      <div className='bg-[#374674] flex justify-center items-center rounded-[10px] md:mt-6 mt-4 md:p-[20px] p-4'>
         <h1 className='text-white font-bold leading-[30px] md:text-[35px] sm:text-[16px]'>Start a Campaign</h1>
       </div>
       <form className='w-full flex flex-col mt-6 gap-4'>
         <div className='flex flex-wrap gap-4'>
           <FormField
-          labelName='Your Name*'
+          labelName='Your Name *'
           placeholder='chola kuboko'
           inputType='text'
           value={form.name}
           handleChange={()=>{}}
           />
           <FormField
-          labelName='Campaign Title*'
+          labelName='Campaign Title *'
           placeholder='Write title for campaign'
           inputType='text'
           value={form.title}
           handleChange={()=>{}}
           />
         </div>
+        <FormField
+          labelName='Story *'
+          placeholder='Write title story'
+          isTextArea
+          value={form.description}
+          handleChange={()=>{}}
+          />
       </form>
     </div>
   )
